c1c57156077d5f93071afb50dd80db478298ae64
Juliano <julianodesenv@gmail.com>
2021-07-31 09:35:42 -0300

    project structure

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import homeRoutes from './src/routes/homeRoutes'
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middleares();
+    this.routes();
+  }
+
+  middleares() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use('/', homeRoutes);
+  }
+}
+
+export default new App().app;
---
+++ src/controllers/HomeController.js
@@ -0,0 +1,9 @@
+class HomeController {
+  index(req, res) {
+    res.json({
+      "tudo certo": true
+    })
+  }
+}
+
+export default new HomeController();
