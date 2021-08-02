c0e574b2a206e75256c3733c2b223ca83323e0fe
Juliano <julianodesenv@gmail.com>
2021-08-02 15:36:15 -0300

    ajustes para publicar

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,17 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
-  async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Juliano',
-      sobrenome: 'Monteiro',
-      email: 'aaa@aa.com',
-      idade: 29,
-      peso: 84,
-      altura: 1.8,
-    });
-
-    res.json(novoAluno);
+  index(req, res) {
+    res.json('Rodando');
   }
 }
 
