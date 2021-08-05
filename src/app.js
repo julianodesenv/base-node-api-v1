3d4d0288943fc8dd42cd8f57602d33bb1641909a
Juliano <julianodesenv@gmail.com>
2021-08-05 10:03:18 -0300

    add cors and helmet api

--- src/app.js
+++ src/app.js
@@ -2,12 +2,29 @@ require('dotenv').config();
 
 import './database';
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 import { resolve } from 'path';
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import photoRoutes from './routes/photoRoutes';
+
+const whitelist = [
+  'https://minhaapi.julianodesenv.com.br',
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   constructor() {
@@ -17,6 +34,9 @@ class App {
   }
 
   middleares() {
+    // this.app.use(cors(corsOptions));
+    this.app.use(cors());
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import app from './app';
 
-const port = process.env.APP_PORT;
+const port = process.env.APP_URL_PORT;
 app.listen(port, () => {
-  // console.log();
-  // console.log(`Escutando na porta ${port}`);
-  // console.log(`CTRL + Clique em http://localhost:${port}`);
+  console.log();
+  console.log(`Escutando na porta ${port}`);
+  console.log(`CTRL + Clique em http://localhost:${port}`);
 });
